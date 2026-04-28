28c4a8dbd69fe49d24e755d121f7cf46e87fc01a
Ana Ibarra <anagabrielaibarra58@gmail.com>
2026-04-28 15:38:00 -0300

    tercer commit

---
+++ Ejercicio2-procesador-lista-invitados/index.js
@@ -0,0 +1,61 @@
+/*
+
+### Ejercicio 2: Procesador de Listas de Invitados
+
+Debes crear una función `procesarListas` que acepte un número variable de listas de invitados (arrays de strings con nombres).
+La función debe:
+1.  Combinar todas las listas en una sola.
+2.  Eliminar nombres duplicados (cada invitado debe aparecer solo una vez).
+3.  Devolver un objeto con dos propiedades:
+    *   `invitadosUnicos`: Un `Set` con los nombres únicos de los invitados.
+    *   `conteoTotalInvitados`: El número total de nombres recibidos antes de la deduplicación.
+    *   `conteoInvitadosUnicos`: El número de invitados únicos.
+
+**Pistas:**
+*   Usa el operador `rest` para aceptar múltiples listas.
+*   Usa el operador `spread` para combinar los arrays.
+*   Usa `Set` para obtener los nombres únicos.
+
+ */
+
+
+function procesarListas(...listasRecibidas) {
+
+    const listaCombinada = [];
+    
+    for (let i = 0; i < listasRecibidas.length; i++) {
+        const listaActual = listasRecibidas[i];
+        
+        for (let j = 0; j < listaActual.length; j++) {
+            listaCombinada.push(listaActual[j]);
+        }
+    }
+
+    let datosLista = ""
+
+    for (let i = 0; i < listaCombinada.length; i++) {
+        datosLista += listaCombinada[i] + ", ";
+    }   
+
+    let totalElementosLista = listaCombinada.length;
+
+    const invitadosUnicos = new Set(listaCombinada);
+
+    let conteoUnicos = invitadosUnicos.size;
+
+    // 5. Devolver el objeto final
+    return {
+        datosLista: datosLista,
+        invitadosUnicos: invitadosUnicos,
+        totalElementosLista: totalElementosLista,
+        conteoInvitadosUnicos: conteoUnicos
+    };
+}
+
+// --- Prueba ---
+const listaInvitados1 = ["Ana", "Gabriela", "Carlos", "Paula"];
+const listaInvitados2 = ["Nicole", "Mariana", "Florencia", "Veronica"];
+
+const resultado = procesarListas(listaInvitados1, listaInvitados2);
+console.log(resultado);
+
